54d4f6c60fb8ff251580251782d35276ca443f28
Diego Orellana <dev@dorellango.cl>
2017-11-13 14:20:59 -0300

    initial commit :tada:

---
+++ components/Hero.vue
@@ -0,0 +1,9 @@
+<template>
+  <div>
+    <h1>Hi, here the Hero!</h1>
+  </div>  
+</template>
+
+<style lang="sass">
+
+</style>
---
+++ layouts/default.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    <nuxt/>
+  </div>
+</template>
+
+<style lang="sass">
+  @import "~assets/sass/app.sass"
+
+  body
+    font-family: "Roboto", arial, sans serif
+</style>
