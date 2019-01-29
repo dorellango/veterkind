7cf873624cdb51fe8537d24769085039710c94e4
Diego Orellana <dev@dorellango.cl>
2019-01-29 14:03:03 -0300

    udpate products info

--- components/products.js
+++ components/products.js
@@ -1,18 +1,18 @@
 let products = [
   {
-    title: 'Porta sueros',
+    title: 'Porta Sueros',
     short_description: 'Estructura en perfil cuadrado de 1.0 mm. [...]',
     description: 'Estructura en perfil cuadrado de 1.0 mm. Pintura Martillado grafito. Bandeja de acero inoxidable. Altura regulable. Dimensiones  BANDEJA 40X30. Color GRIS. Marca Veterkind ®, hecho en CHILE.',
     image: 'porta-sueros.jpg'
   },
   {
-    title: 'Camilla transporte veterinaria plegable',
+    title: 'Camilla Transporte Veterinaria Plegable',
     short_description: 'Mesa-camilla de exploración plegable con ruedas. [...]',
     description: 'Mesa-camilla de exploración plegable con ruedas. Estructura 100% metálica, ultra liviana, inoxidable. Fabricación propia OSM Ltda. de la línea muebles clínicos Veterkind®. Resiste hasta 90 kilos. Superficie de lona ultra resistente, impermeable y lavable. Ruedas giratorias de alto tráfico con freno. Sujeción, correas con velcro para seguridad y control animal. Plegable para mayor comodidad. Garantía 1 año. Largo: 100cm, Ancho: 50cm, Alto: 80cm. Color según disponibilidad.',
     image: 'mesa-plegable-mayo.jpg'
   },
   {
-    title: 'Unidad Quirúrgica veterinaria móvil',
+    title: 'Unidad Quirúrgica Veterinaria Móvil',
     short_description: 'Mesa de cirugía y procedimiento modelo premium [...]',
     description: 'Totalmente plegable, tipo maleta, para trasladar cómodamente a terreno u operativos. Para uso en clínica, cirugía, procedimientos, baño sanitario. Ruedas para traslado cómodo. Arco de sujeción para baño sanitario apto peluquería, procedimientos y examen clínico. Cubierta de acero inoxidable en 1mm. Acero grado médico. Medidas: 1.20 x 60 cm. Altura regulable desde 85 cm a 1.0 mt. Con canal diamantado y orificio de drenaje. Recipiente para fluidos de acero, movible y desmontable. Porta bandejas movible en 360°. Bandeja de acero inoxidable, para instrumentos quirúrgicos desmontables. Bandeja para ayudante, desmontable. Estructura 100% metálica. Porta suero 4 ganchos, desmontable y regulable en altura. Lámpara clínica con cuello flexible, todas las posiciones desmontables. Manijas laterales para fijación de paciente. Manilla para traslado. 4 ruedas alta resistencia, c/u con freno independiente. Arco regulable en altura hasta 88 cm. largo 1.25 mt. Arco incluye 2 ganchos de corredera independiente, con freno fijador c/u. Garantía: 5 años contra defectos de fabricación.',
     image: 'mesa-premium.jpg'
@@ -24,13 +24,13 @@ let products = [
     image: 'caniles-modulares.jpg'
   },
   {
-    title: 'Mesa Quirúrgica completa',
+    title: 'Mesa Quirúrgica Completa',
     short_description: 'Cubierta en acero inoxidable grado médico, plegable [...]',
     description: 'Totalmente Plegable. Estructura 100% metálica, medidas 1.20 de largo x 60 de ancho y 85 de alto regulable hasta 1, 0 mt. Manijas laterales para fijación de paciente, manilla para traslado y canal diamantado y orificio de drenaje. Porta bandeja movible en 360º, porta suero con 4 ganchos desmontable, lámpara clínica con cuello flexible, recipiente de acero para fluidos. Lámpara clínica con cuello flexible todas las posiciones desmontable.',
     image: 'quirurgica-completa.jpg'
   },
   {
-    title: 'Mesa de Examen básica',
+    title: 'Mesa de Examen Básica',
     short_description: 'Cubierta en acero inoxidable grado médico, plegable [...]',
     description: 'Totalmente Plegable. Estructura 100% metálica, medidas 1.20 de largo x 60 de ancho y 85 de alto regulable hasta 1, 0 mt. Manijas laterales para fijación de paciente, manilla para traslado y canal diamantado y orificio de drenaje. Regulador de alzador de funcionamiento independiente(en caso que la mesa esté ubicada en superficie irregular, lo que le a nos centímetros más de altura). Cubierta en acero inoxidable grado médico.',
     image: 'quirurgica-basica.jpg'
