e19ef3ca3f203805b950ea0c1a417a10ad3459ed
Diego Orellana <dev@dorellango.cl>
2019-03-29 12:50:23 -0300

    fix wrong typed excerpt

--- components/products.js
+++ components/products.js
@@ -7,7 +7,7 @@ let products = [
   // },
   {
     title: 'Mesa Quirúrgica Veterinaria 3600',
-    short_description: '•Elevación con ajuste hidráulico hasta 45º (Trendelenburg) [...]',
+    short_description: 'Elevación con ajuste hidráulico hasta 45º (Trendelenburg) [...]',
     description: 'Totalmente Plegable. Cubierta en acero inoxidable grado médico. Elevación con ajuste hidráulico hasta 45º (Trendelenburg). Con canal diamantado y orificio de drenaje. Porta recipiente movible y desmontable. Recipiente de acero para fluidos . Porta bandeja movible en 360º . Bandeja de acero inoxidable para instrumentos quirúrgicos desmontable. Bandeja para ayudante desmontable. Brazo acero para cogotera, altura regulable, desmontable. Estructura 100% metálica . Porta suero con 4 ganchos desmontable. Porta monitor multiparámetros ajustable en altura y sistema seguridad anti caídas (monitor no incluido), desmontable. Lámpara clínica con cuello flexible todas las posiciones desmontable. Ampolleta iluminación LED incluida. 1.20 de largo x 60 de ancho y 85 de alto. Mesa con altura regulable hasta 1 metro. Manijas laterales para fijación de paciente. Ruedas con freno de alta resistencia. Plegable para uso en terreno u operativos veterinarios. Todos los accesorios desmontables e intercambiables de posición. Manilla para traslado. Estructura perfil de 1mm. Pintura electroestática al horno. Garantía 5 años. Marca Veterkind®, hecho en CHILE.',
     image: 'mesa-3600.jpg',
     is_new: true
